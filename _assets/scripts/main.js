dbc614c2149924cfb89d437604f2d63201a0d673
zemna <zemna@zemna.net>
2016-07-05 21:24:52 +0700

    Cleaning code

--- _assets/scripts/main.js
+++ _assets/scripts/main.js
@@ -9,13 +9,7 @@
             };
         };
 
-        this.registerGlobals();
         this.addListeners();
-        this.activateSearch();
-    };
-
-    App.registerGlobals = function() {
-
     };
 
     App.addListeners = function() {
@@ -33,14 +27,12 @@
             var activeItem = navItem.find('active');
 
             switch (currentUrl) {
-                case 'projects':
+                case 'categories':
+                    navItem.eq(0).addClass('active');
+                    break;
+                case 'tags':
                     navItem.eq(1).addClass('active');
                     break;
-                case 'guest-book':
-                    navItem.eq(2).addClass('active');
-                    break;
-                default:
-                    navItem.eq(0).addClass('active');
             }
 
             activeItem.removeClass('active');
@@ -49,20 +41,6 @@
             bodyContainer.find('table')
                 .addClass('table')
                 .wrap('<div class="table-responsive"></div>');
-
-            /* Create and attach an id anchor for each titles */
-            //var boxes = document.getElementById('main').getElementsByClassName('box-body');
-            var len = bodyContainer.length;
-
-            if (len < 1) {
-                return false;
-            }
-
-            for (var i = 0; i < len; i++) {
-                for (var level = 1; level <= 3; level++) {
-                    self.linkifyAnchors(level, bodyContainer[i]);
-                }
-            }
 
             /* Activate back-to-top button */
             topButton.on('click', function () {
@@ -109,72 +87,6 @@
                 topButton.fadeOut();
             }
         });
-
-        /* Generate pop-up window for SNS sharing */
-        $('#sns-facebook').on('click', function(e) {
-            e.preventDefault();
-            window.open($(this).attr('href'), 'sns-facebook','width=580,height=296');
-        });
-
-        $('#sns-twitter').on('click', function(e) {
-            e.preventDefault();
-            window.open($(this).attr('href'), 'sns-twitter','width=550,height=235');
-        });
-
-        $('#sns-google').on('click', function(e) {
-            e.preventDefault();
-            window.open($(this).attr('href'), 'sns-google','width=490,height=530');
-        });
-    };
-
-    /* Helpers */
-
-    App.anchorForId = function (id) {
-        var anchorElem = document.createElement('a');
-
-        anchorElem.className = 'header-link';
-        anchorElem.href = '#' + id;
-        anchorElem.innerHTML = '<span class="sr-only">Permalink</span><i class="material-icons">link</i>';
-        anchorElem.title = 'Permalink';
-
-        return anchorElem;
-    };
-
-    App.linkifyAnchors = function (level, containingElement) {
-        var headers = containingElement.getElementsByTagName('h' + level);
-        headers = Array.prototype.slice.call(headers);
-        headers = headers.filter(function(v, i) {
-            return v.parentElement === containingElement;
-        });
-
-        for (var h = 0; h < headers.length; h++) {
-            var header = headers[h];
-            var slug   = this.slugify(this.purify(header.childNodes));
-
-            if (typeof slug !== 'undefined' && slug !== '') {
-                header.id = slug;
-                header.appendChild(this.anchorForId(slug));
-            }
-        }
-    };
-
-    App.purify = function (node) {
-        for (var i = 0, len = node.length; i < len; i++) {
-            if (node.item(i).nodeName == '#text') {
-                return node.item(i).textContent;
-            }
-        }
-
-        return 'unknown-' + Math.round(Math.random() * 100000);
-    };
-
-    App.slugify = function (text) {
-        return text.toString().toLowerCase()
-            .replace(/[~!@#\$%\^&\*\(\)\[\]\{\}\/:;"',\.<>?]/g, '') // Replace special chars
-            .replace(/\s+/g, '-')           // Replace spaces with -
-            .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-            .replace(/^-+/, '')             // Trim - from start of text
-            .replace(/-+$/, '');            // Trim - from end of text
     };
 
     $(function() {
@@ -214,6 +126,7 @@ var compile = function(layoutTemplate, facebookUserFeedCollection) {
   return markup;
 };
 
+/* Redirect to https */
 if (!(window.location.host.startsWith('127.0.0.1') || window.location.host.startsWith('localhost')) && (window.location.protocol != 'https:')) {
     window.location.protocol = 'https';
 }
